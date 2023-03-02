c9942e25809dc4778af70ac9421e1ed48fc4b964
Lgibb18 <grigoriy.simonov.2010@gmail.com>
2023-03-02 20:50:31 +0500

    oasss

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class aclient(discord.Client):
             await tree.sync()
             self.synced = True
         print(f"We have logged in as {self.user}.")
+        bot.get_guild(720950193700208671).get_channel(891255796564905994).send("Бот запущен")
 
 client = aclient()
 tree = app_commands.CommandTree(client)
