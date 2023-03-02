2762f5f2c97cdd255e7d7fbdf23e5ffd26060dc9
Lgibb18 <grigoriy.simonov.2010@gmail.com>
2023-03-02 20:46:15 +0500

    fix

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ async def slash(interaction:discord.Interaction):
         g = git.cmd.Git(Repo.working_tree_dir)
         g.pull()
         await interaction.response.send_message("Бот перезапустится в течении 10 секунд")
+        await asyncio.sleep(5)
         os.execl(sys.executable, sys.executable, *sys.argv)
         
         #subprocess.call("start.bat")
