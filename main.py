886b12ae2f588f686fc0a41fe69f5622b8c3d1d7
Lgibb18 <grigoriy.simonov.2010@gmail.com>
2023-03-03 08:23:47 +0500

    h

--- main.py
+++ main.py
@@ -247,7 +247,8 @@ async def slash(interaction:discord.Interaction,
         embed.add_field(name=f"Причина: {_reason}", value=f"", inline=False)
         #await interaction.response.send_message(embed=embed)
         
-        
+        if(not _inlb):
+            embed.set_footer("Не зачислено в лидерборд")
         
         await guild.get_channel(724614253079822418).send(embed=embed)
         await guild.get_channel(846747716348018750).send(embed=embed)
